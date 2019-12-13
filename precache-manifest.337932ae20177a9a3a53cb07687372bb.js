31923470a8f917edd00852fa7d57f8d5b5d5229a
beejaybee <oyewob@gmail.com>
2019-12-13 15:49:50 +0100

    Updates

---
+++ precache-manifest.337932ae20177a9a3a53cb07687372bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2347694827badf0eefdd51fe3ab6adb4",
+    "url": "/api-pics/index.html"
+  },
+  {
+    "revision": "14bdb129472fcb8019b7",
+    "url": "/api-pics/static/css/main.65e3c85d.chunk.css"
+  },
+  {
+    "revision": "3b810d0bbcef2f4a16e2",
+    "url": "/api-pics/static/js/2.c4149a96.chunk.js"
+  },
+  {
+    "revision": "14bdb129472fcb8019b7",
+    "url": "/api-pics/static/js/main.144e819f.chunk.js"
+  },
+  {
+    "revision": "5a614a62b08ff0107f97",
+    "url": "/api-pics/static/js/runtime-main.0d7a76eb.js"
+  }
+]);
